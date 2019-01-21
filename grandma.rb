3f81ac77a3c8ab3a2ddcff3854cfaf790ba90a0e
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-21 16:41:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma
   word = gets.chomp
   if word == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else if word = capitals
+  else if word = word.capitalize
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
